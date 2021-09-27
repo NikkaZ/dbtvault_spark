a4555cf21be600e59ad015bff89497fc64bcfc26
Nikka Zanandrea <nikka.zanandrea@nhs.net>
2021-09-27 15:34:56 +0100

    handle struct data

--- dbtvault-dev/macros/staging/source_columns.sql
+++ dbtvault-dev/macros/staging/source_columns.sql
@@ -13,3 +13,20 @@
     {%- endif %}
 
 {%- endmacro -%}
+
+{%- macro source_columns_from_selected(source_relation=none,selected_columns=none) -%}
+
+    {%- if source_relation -%}
+        {%- set source_model_cols = adapter.get_columns_in_relation(source_relation) -%}
+        {%- set column_list = [] -%}
+
+        {%- for source_col in source_model_cols -%}
+            {% if source_col.column in selected_columns %}
+                {%- do column_list.append(source_col.column) -%}
+            {%- endif %}
+        {%- endfor -%}
+
+        {%- do return(column_list) -%}
+    {%- endif %}
+
+{%- endmacro -%}
