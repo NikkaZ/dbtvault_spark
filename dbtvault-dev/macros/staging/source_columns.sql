98f31bf6a4b5db7422ba74bcb7b4b704698ee3b4
Nikka Zanandrea <nikka.zanandrea@nhs.net>
2021-09-29 09:55:50 +0100

    allow selected_columns none

--- dbtvault-dev/macros/staging/source_columns.sql
+++ dbtvault-dev/macros/staging/source_columns.sql
@@ -16,18 +16,22 @@
 
 {%- macro source_columns_from_selected(source_relation=none,selected_columns=none) -%}
 
-    {%- if source_relation -%}
-        {%- set source_model_cols = adapter.get_columns_in_relation(source_relation) -%}
-        {%- set column_list = [] -%}
+    {% if selected_columns is none %}
+        {%- set column_list = dbtvault.source_columns(source_relation=source_relation) -%}
+    {%- else %}
+        {%- if source_relation -%}
+            {%- set source_model_cols = adapter.get_columns_in_relation(source_relation) -%}
+            {%- set column_list = [] -%}
 
-        {%- for source_col in source_model_cols -%}
-            {# added not in cause some struct data has the same field inside. Example gender [struct<scn:int,from:int,to:int,gender:string>]#}
-            {% if source_col.column in selected_columns and source_col.column not in column_list %}
-                {%- do column_list.append(source_col.column) -%}
-            {%- endif %}
-        {%- endfor -%}
+            {%- for source_col in source_model_cols -%}
+                {# added not in cause some struct data has the same field inside. Example gender [struct<scn:int,from:int,to:int,gender:string>]#}
+                {% if source_col.column in selected_columns and source_col.column not in column_list %}
+                        {%- do column_list.append(source_col.column) -%}
+                {%- endif %}
+            {%- endfor -%}
 
-        {%- do return(column_list) -%}
+        {%- endif %}
     {%- endif %}
+    {%- do return(column_list) -%}
 
 {%- endmacro -%}
