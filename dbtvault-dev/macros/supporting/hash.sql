a357cc8f7f81a2460043dbb54e4069510a684c61
Nikka Zanandrea <nikka.zanandrea@nhs.net>
2022-05-26 09:00:00 +0100

    adjustments after merging

--- dbtvault-dev/macros/supporting/hash.sql
+++ dbtvault-dev/macros/supporting/hash.sql
@@ -232,3 +232,68 @@
 {%- endif -%}
 
 {%- endmacro -%}
+
+{%- macro spark__hash(columns, alias, is_hashdiff) -%}
+
+{%- set hash = var('hash', 'MD5') -%}
+{%- set concat_string = var('concat_string', '||') -%}
+{%- set null_placeholder_string = var('null_placeholder_string', '^^') -%}
+
+{#- Select hashing algorithm -#}
+{%- if hash == 'MD5' -%}
+    {%- set hash_alg = 'MD5' -%}
+    {%- set hash_size = '' -%}
+{%- elif hash == 'SHA' -%}
+    {%- set hash_alg = 'SHA2' -%}
+    {%- set hash_size = ',256' -%}
+{%- else -%}
+    {%- set hash_alg = 'MD5' -%}
+    {%- set hash_size = '' -%}
+{%- endif -%}
+
+{%- set standardise = "NULLIF(UPPER(TRIM(CAST([EXPRESSION] AS STRING))), '')" %}
+
+{#- Alpha sort columns before hashing if a hashdiff -#}
+{%- if is_hashdiff and dbtvault.is_list(columns) -%}
+    {%- set columns = columns|sort -%}
+{%- endif -%}
+
+{#- If single column to hash -#}
+{%- if columns is string -%}
+    {%- set column_str = dbtvault.as_constant(columns) -%}
+    {{- "CAST({}({} {}) AS BINARY) AS {}".format(hash_alg, standardise | replace('[EXPRESSION]', column_str), hash_size, alias) | indent(4) -}}
+{#- Else a list of columns to hash -#}
+{%- else -%}
+    {%- set all_null = [] -%}
+
+    {%- if is_hashdiff -%}
+        {{- "CAST({}(CONCAT_WS('{}',".format(hash_alg, concat_string) | indent(4) -}}
+    {%- else -%}
+        {{- "CAST({}(NULLIF(CONCAT_WS('{}',".format(hash_alg, concat_string) | indent(4) -}}
+    {%- endif -%}
+
+    {%- for column in columns -%}
+
+        {%- do all_null.append(null_placeholder_string) -%}
+
+        {%- set column_str = dbtvault.as_constant(column) -%}
+        {{- "\nIFNULL({}, '{}')".format(standardise | replace('[EXPRESSION]', column_str), null_placeholder_string) | indent(4) -}}
+        {{- "," if not loop.last -}}
+
+        {%- if loop.last -%}
+            {% if is_hashdiff %}
+                {{- "\n) {}) AS BINARY) AS {}".format(hash_size, alias) -}}
+            {%- else -%}
+                {{- "\n), '{}'){}) AS BINARY) AS {}".format(all_null | join(""), hash_size, alias) -}}
+            {%- endif -%}
+        {%- else -%}
+
+            {%- do all_null.append(concat_string) -%}
+
+        {%- endif -%}
+
+    {%- endfor -%}
+
+{%- endif -%}
+
+{%- endmacro -%}
